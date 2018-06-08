668db2e3eda2991a791676be2b42da4607dcfd78
Artur Grigio <arturgrigio@gmail.com>
2018-06-08 16:39:31 -0700

    added the enums

---
+++ src/enums/NotificationActions.ts
@@ -0,0 +1,11 @@
+/**
+ * Sets up the the notification action states
+ *
+ * @export
+ * @enum {string}
+ */
+enum NotificationActions {
+  MODAL_CLOSED = 'MODAL_CLOSED',
+  MODAL_OPENED = 'MODAL_OPENED',
+}
+export default NotificationActions;
---
+++ src/enums/NotificationStatus.ts
@@ -0,0 +1,13 @@
+/**
+ * Sets up the status of the notification
+ *
+ * @export
+ * @enum {string}
+ */
+enum NotificationStatus {
+  SUCCESS = 'SUCCESS',
+  INFO = 'INFO',
+  WARNING = 'WARNING',
+  ERROR = 'ERROR',
+}
+export default NotificationStatus;
